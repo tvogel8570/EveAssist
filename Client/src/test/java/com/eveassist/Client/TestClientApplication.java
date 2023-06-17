1bed28772a1956a8fb06e3da2a0dfeb3234ad487
Timothy Vogel <Timothy@vogelware.com>
2023-06-16 19:27:24 -0500

    import starter files from Spring Initializr

---
+++ Client/src/test/java/com/eveassist/Client/TestClientApplication.java
@@ -0,0 +1,13 @@
+package com.eveassist.Client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(ClientApplication::main).with(TestClientApplication.class).run(args);
+	}
+
+}
