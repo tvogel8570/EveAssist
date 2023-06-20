b6442b6b7808cb4f3a7473b06e26ea8ba998ed23
Timothy Vogel <Timothy@vogelware.com>
2023-06-20 04:01:59 -0500

    change to lowercase client

--- Client/src/test/java/com/eveassist/client/ClientApplicationTests.java
+++ Client/src/test/java/com/eveassist/client/ClientApplicationTests.java
@@ -1,4 +1,4 @@
-package com.eveassist.Client;
+package com.eveassist.client;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ClientApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @SuppressWarnings("squid:S2699")
+    @Test
+    void contextLoads() {
+    }
 }
--- Client/src/test/java/com/eveassist/client/TestClientApplication.java
+++ Client/src/test/java/com/eveassist/client/TestClientApplication.java
@@ -1,4 +1,4 @@
-package com.eveassist.Client;
+package com.eveassist.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.TestConfiguration;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.TestConfiguration;
 @TestConfiguration(proxyBeanMethods = false)
 public class TestClientApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.from(ClientApplication::main).with(TestClientApplication.class).run(args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.from(ClientApplication::main).with(TestClientApplication.class).run(args);
+    }
 
 }
