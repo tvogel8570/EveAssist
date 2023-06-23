288c3e3319e5dfdf6aa949e55087ec81278f376d
Timothy Vogel <Timothy@vogelware.com>
2023-06-23 10:20:58 -0500

    import project structure from Taming Thymeleaf book

--- Client/src/main/java/com/eveassist/client/config/UiConfig.java
+++ Client/src/main/java/com/eveassist/client/config/UiConfig.java
@@ -3,9 +3,21 @@ package com.eveassist.client.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 
 @Configuration
 public class UiConfig {
+    @Bean
+    public ITemplateResolver svgTemplateResolver() {
+        SpringResourceTemplateResolver resolver = new
+                SpringResourceTemplateResolver();
+        resolver.setPrefix("classpath:/templates/svg/");
+        resolver.setSuffix(".svg");
+        resolver.setTemplateMode("XML");
+        return resolver;
+    }
+
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
---
+++ Client/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+    content: ['./src/main/resources/templates/**/*.html',
+        './src/main/resources/templates/**/*.svg'],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+    plugins: [
+        require('@tailwindcss/forms')],
+}
+
