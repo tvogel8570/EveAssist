04f92e41346f741b00389afe2399d51003ae1dbe
Timothy Vogel <timothy@vogelware.com>
2023-06-30 22:08:00 -0400

    added test to stop SonarLint from complaining

--- Api/src/test/java/com/eveassist/api/ApiApplicationTests.java
+++ Api/src/test/java/com/eveassist/api/ApiApplicationTests.java
@@ -1,13 +1,19 @@
 package com.eveassist.api;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.RestTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class ApiApplicationTests {
+    @Autowired
+    RestTemplate restTemplate;
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        assertThat(restTemplate).isNotNull();
+    }
 }
