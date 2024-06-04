14c113742242982842260fd3f578521faa4431d7
Timothy Vogel <Timothy@vogelware.com>
2024-06-04 17:53:41 -0500

    add ability to print out all beans

--- Client/src/main/java/com/eveassist/client/ClientApplication.java
+++ Client/src/main/java/com/eveassist/client/ClientApplication.java
@@ -2,12 +2,21 @@ package com.eveassist.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class ClientApplication {
+    private static ApplicationContext applicationContext;
 
     public static void main(String[] args) {
-        SpringApplication.run(ClientApplication.class, args);
+        applicationContext = SpringApplication.run(ClientApplication.class, args);
+//        displayAllBeans();
     }
 
+    public static void displayAllBeans() {
+        String[] allBeanNames = applicationContext.getBeanDefinitionNames();
+        for (String beanName : allBeanNames) {
+            System.out.println(beanName);
+        }
+    }
 }
