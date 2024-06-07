115dfb1cf915648da88704ef6d0026925c8e662a
Timothy Vogel <Timothy@vogelware.com>
2024-06-07 15:49:27 -0400

    WIP

--- Api/src/main/java/com/eveassist/api/config/ApiConfig.java
+++ Api/src/main/java/com/eveassist/api/config/ApiConfig.java
@@ -4,9 +4,12 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.format.DateTimeFormatter;
@@ -17,9 +20,18 @@ public class ApiConfig {
     private static final LocalDateTimeSerializer LOCAL_DATETIME_SERIALIZER =
             new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DATETIME_FORMAT));
 
+    @Value("${esi.baseUrl}")
+    private String ESI_BASE_PATH;
+
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    @Qualifier("EsiRestClient")
+    public RestClient esiRestClient() {
+        return RestClient.builder().baseUrl(ESI_BASE_PATH).build();
     }
 
     @Bean
